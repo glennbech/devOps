053fd3f4d34195536a327324a2d1cf9be42851c3
henaaro <114416967+henaaro@users.noreply.github.com>
2024-11-18 22:46:27 +0100

    noe skjer!

---
+++ sam_lambda/generate-image-lambda/app/app.py
@@ -0,0 +1,51 @@
+import base64
+import boto3
+import json
+import os
+import random
+
+# Sett opp AWS-klienter
+bedrock_client = boto3.client("bedrock-runtime", region_name="us-east-1")
+s3_client = boto3.client("s3")
+
+# Hent dynamisk bucket-navnet fra miljøvariabel
+bucket_name = os.environ["S3_BUCKET_NAME"]
+
+def lambda_handler(event, context):
+    # Hent prompt fra HTTP POST body
+    body = json.loads(event["body"])
+    prompt = body["prompt"]
+
+    seed = random.randint(0, 2147483647)
+    s3_image_path = f"generated_images/titan_{seed}.png"
+
+    # Bygg forespørselen for generering av bildet
+    native_request = {
+        "taskType": "TEXT_IMAGE",
+        "textToImageParams": {"text": prompt},
+        "imageGenerationConfig": {
+            "numberOfImages": 1,
+            "quality": "standard",
+            "cfgScale": 8.0,
+            "height": 1024,
+            "width": 1024,
+            "seed": seed,
+        }
+    }
+
+    # Kall til Bedrock API for bilde-generering
+    response = bedrock_client.invoke_model(modelId="amazon.titan-image-generator-v1", body=json.dumps(native_request))
+    model_response = json.loads(response["body"].read())
+
+    # Ekstraher og dekod base64-bildedata
+    base64_image_data = model_response["images"][0]
+    image_data = base64.b64decode(base64_image_data)
+
+    # Last opp bildet til S3-bucket
+    s3_client.put_object(Bucket=bucket_name, Key=s3_image_path, Body=image_data)
+
+    # Returner en suksessrespons
+    return {
+        "statusCode": 200,
+        "body": json.dumps({"imageUrl": f"s3://{bucket_name}/{s3_image_path}"})
+    }
